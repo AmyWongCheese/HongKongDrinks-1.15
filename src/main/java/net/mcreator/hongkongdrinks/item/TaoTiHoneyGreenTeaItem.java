44ddc7d8fc881b258f95a83678522ff81a648de1
AmyWongCheese <mmwong@MMWONG-PC.jccsstm.local>
2020-05-29 15:45:19 +0800

    honey green

--- src/main/java/net/mcreator/hongkongdrinks/item/TaoTiHoneyGreenTeaItem.java
+++ src/main/java/net/mcreator/hongkongdrinks/item/TaoTiHoneyGreenTeaItem.java
@@ -4,13 +4,17 @@ package net.mcreator.hongkongdrinks.item;
 import net.minecraftforge.registries.ObjectHolder;
 
 import net.minecraft.world.World;
+import net.minecraft.util.Hand;
+import net.minecraft.util.ActionResult;
 import net.minecraft.item.UseAction;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
 import net.minecraft.item.Food;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.LivingEntity;
 
 import net.mcreator.hongkongdrinks.procedures.VitaLemonTeaFoodEatenProcedure;
+import net.mcreator.hongkongdrinks.procedures.TaoTiHoneyGreenTeaOnFoodRightClickedProcedure;
 import net.mcreator.hongkongdrinks.itemgroup.HongKongDrinksItemGroup;
 import net.mcreator.hongkongdrinks.HongkongdrinksModElements;
 
@@ -36,6 +40,25 @@ public class TaoTiHoneyGreenTeaItem extends HongkongdrinksModElements.ModElement
 		@Override
 		public UseAction getUseAction(ItemStack par1ItemStack) {
 			return UseAction.DRINK;
+		}
+
+		@Override
+		public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity entity, Hand hand) {
+			ActionResult<ItemStack> ar = super.onItemRightClick(world, entity, hand);
+			ItemStack itemstack = ar.getResult();
+			int x = (int) entity.getPosX();
+			int y = (int) entity.getPosY();
+			int z = (int) entity.getPosZ();
+			{
+				java.util.HashMap<String, Object> $_dependencies = new java.util.HashMap<>();
+				$_dependencies.put("entity", entity);
+				$_dependencies.put("x", x);
+				$_dependencies.put("y", y);
+				$_dependencies.put("z", z);
+				$_dependencies.put("world", world);
+				TaoTiHoneyGreenTeaOnFoodRightClickedProcedure.executeProcedure($_dependencies);
+			}
+			return ar;
 		}
 
 		@Override

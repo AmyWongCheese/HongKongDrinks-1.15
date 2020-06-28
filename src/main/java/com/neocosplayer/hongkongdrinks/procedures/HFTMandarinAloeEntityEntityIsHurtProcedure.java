76427991ab8cf003c62f345ecdc015fcd35d24d3
AmyWongCheese <wong@wong-PC>
2020-06-28 22:58:43 +0800

    HFTMandarinAloe

---
+++ src/main/java/com/neocosplayer/hongkongdrinks/procedures/HFTMandarinAloeEntityEntityIsHurtProcedure.java
@@ -0,0 +1,68 @@
+package com.neocosplayer.hongkongdrinks.procedures;
+
+import net.minecraft.world.World;
+import net.minecraft.world.GameType;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.client.network.play.NetworkPlayerInfo;
+import net.minecraft.client.entity.player.ClientPlayerEntity;
+import net.minecraft.client.Minecraft;
+
+import com.neocosplayer.hongkongdrinks.item.HFTMandarinAloeItem;
+import com.neocosplayer.hongkongdrinks.HongkongdrinksModElements;
+
+@HongkongdrinksModElements.ModElement.Tag
+public class HFTMandarinAloeEntityEntityIsHurtProcedure extends HongkongdrinksModElements.ModElement {
+	public HFTMandarinAloeEntityEntityIsHurtProcedure(HongkongdrinksModElements instance) {
+		super(instance, 229);
+	}
+
+	public static void executeProcedure(java.util.HashMap<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			System.err.println("Failed to load dependency entity for procedure HFTMandarinAloeEntityEntityIsHurt!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			System.err.println("Failed to load dependency x for procedure HFTMandarinAloeEntityEntityIsHurt!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			System.err.println("Failed to load dependency y for procedure HFTMandarinAloeEntityEntityIsHurt!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			System.err.println("Failed to load dependency z for procedure HFTMandarinAloeEntityEntityIsHurt!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			System.err.println("Failed to load dependency world for procedure HFTMandarinAloeEntityEntityIsHurt!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		int x = (int) dependencies.get("x");
+		int y = (int) dependencies.get("y");
+		int z = (int) dependencies.get("z");
+		World world = (World) dependencies.get("world");
+		entity.remove();
+		if ((!(new Object() {
+			public boolean checkGamemode(Entity _ent) {
+				if (_ent instanceof ServerPlayerEntity) {
+					return ((ServerPlayerEntity) _ent).interactionManager.getGameType() == GameType.CREATIVE;
+				} else if (_ent instanceof ClientPlayerEntity) {
+					NetworkPlayerInfo _npi = Minecraft.getInstance().getConnection()
+							.getPlayerInfo(((ClientPlayerEntity) _ent).getGameProfile().getId());
+					return _npi != null && _npi.getGameType() == GameType.CREATIVE;
+				}
+				return false;
+			}
+		}.checkGamemode(entity)))) {
+			if (!world.isRemote) {
+				ItemEntity entityToSpawn = new ItemEntity(world, x, y, z, new ItemStack(HFTMandarinAloeItem.block, (int) (1)));
+				entityToSpawn.setPickupDelay(10);
+				world.addEntity(entityToSpawn);
+			}
+		}
+	}
+}

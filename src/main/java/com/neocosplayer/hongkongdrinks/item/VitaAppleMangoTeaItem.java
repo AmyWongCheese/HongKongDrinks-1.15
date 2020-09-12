282e7a2b7b6f844cf4cd2a36a7cfe53ae47e6b07
AmyWongCheese <mmwong@MMWONG-PC.jccsstm.local>
2020-09-12 11:03:04 +0800

    apple mango

---
+++ src/main/java/com/neocosplayer/hongkongdrinks/item/VitaAppleMangoTeaItem.java
@@ -0,0 +1,86 @@
+
+package com.neocosplayer.hongkongdrinks.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.Hand;
+import net.minecraft.util.ActionResult;
+import net.minecraft.item.UseAction;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.Food;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.LivingEntity;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import com.neocosplayer.hongkongdrinks.procedures.VitaLemonTeaFoodEatenProcedure;
+import com.neocosplayer.hongkongdrinks.procedures.VitaAppleMangoTeaRightClickedInAirProcedure;
+import com.neocosplayer.hongkongdrinks.itemgroup.HongKongDrinksItemGroup;
+import com.neocosplayer.hongkongdrinks.HongkongdrinksModElements;
+
+@HongkongdrinksModElements.ModElement.Tag
+public class VitaAppleMangoTeaItem extends HongkongdrinksModElements.ModElement {
+	@ObjectHolder("hongkongdrinks:vita_apple_mango_tea")
+	public static final Item block = null;
+	public VitaAppleMangoTeaItem(HongkongdrinksModElements instance) {
+		super(instance, 253);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new FoodItemCustom());
+	}
+	public static class FoodItemCustom extends Item {
+		public FoodItemCustom() {
+			super(new Item.Properties().group(HongKongDrinksItemGroup.tab).maxStackSize(64)
+					.food((new Food.Builder()).hunger(4).saturation(0.3f).build()));
+			setRegistryName("vita_apple_mango_tea");
+		}
+
+		@Override
+		public UseAction getUseAction(ItemStack par1ItemStack) {
+			return UseAction.DRINK;
+		}
+
+		@Override
+		public net.minecraft.util.SoundEvent getEatSound() {
+			return net.minecraft.util.SoundEvents.ENTITY_GENERIC_DRINK;
+		}
+
+		@Override
+		public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity entity, Hand hand) {
+			ActionResult<ItemStack> ar = super.onItemRightClick(world, entity, hand);
+			ItemStack itemstack = ar.getResult();
+			double x = entity.getPosX();
+			double y = entity.getPosY();
+			double z = entity.getPosZ();
+			{
+				Map<String, Object> $_dependencies = new HashMap<>();
+				$_dependencies.put("entity", entity);
+				$_dependencies.put("x", x);
+				$_dependencies.put("y", y);
+				$_dependencies.put("z", z);
+				$_dependencies.put("world", world);
+				VitaAppleMangoTeaRightClickedInAirProcedure.executeProcedure($_dependencies);
+			}
+			return ar;
+		}
+
+		@Override
+		public ItemStack onItemUseFinish(ItemStack itemStack, World world, LivingEntity entity) {
+			ItemStack retval = super.onItemUseFinish(itemStack, world, entity);
+			double x = entity.getPosX();
+			double y = entity.getPosY();
+			double z = entity.getPosZ();
+			{
+				Map<String, Object> $_dependencies = new HashMap<>();
+				$_dependencies.put("entity", entity);
+				VitaLemonTeaFoodEatenProcedure.executeProcedure($_dependencies);
+			}
+			return retval;
+		}
+	}
+}
